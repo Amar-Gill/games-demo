fc54f23933029b3ffab511f96497ffdf5618a6e0
Amar Gill <amar.s.gill@outlook.com>
2020-04-14 02:15:43 -0400

    Can render basic grid elements for Add game.

--- src/Add.svelte
+++ src/Add.svelte
@@ -1,48 +1,42 @@
-<div>
-    <h1>Add two numbers: {a} + {b}</h1>
-    <p>What's the correct answer?</p>
+<div class='top-div'>
+    <div>
+        <h1>Add two numbers: {a} + {b}</h1>
+        <p>What's the correct answer?</p>
+    </div>
 
-    {#each options as option}
-    <input type="button" value={option} on:click|preventDefault={validateAnswer}>
-    {/each}
+    <GridBox cards={a} />
+    <GridBox cards={b} />
+
+    <section>
+        {#each options as option}
+            <input type="button" value={option} on:click|preventDefault={validateAnswer}>
+        {/each}
+    </section>
     
 </div>
 
+<style>
+    .top-div {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        height: 100%;
+    }
+
+    .top-div div {
+        border: solid black 2px;
+    }
+
+    section {
+        border: solid black 2px;
+        display: flex;
+        justify-content: space-evenly;
+    }
+</style>
+
 <script>
-    import { onMount, beforeUpdate } from 'svelte';
-
-    // initialize a and b as random ints between 1 and 10
-    let a = Math.floor(Math.random() * 11);
-    let b = Math.floor(Math.random() * 11);
-
-    // set a and b to 1 if they are 0
-    if (a == 0) a = 1;
-    if (b == 0) b = 1;
-
-    // calculate answer
-    const answer = a + b;
-
-    // calculate alternate options in same manner as a and b
-    let alt1 = Math.floor(Math.random() * 11) + Math.floor(Math.random() * 11);
-    let alt2 = Math.floor(Math.random() * 11) + Math.floor(Math.random() * 11);
-
-    // check they are not 0
-    if (alt1 == 0) alt1 = 1;
-    if (alt2 == 0) alt2 = 1;
-
-    // check that alternate options do not equal answer
-    while (alt1 == answer || alt1 == alt2) {
-        alt1 = Math.floor(Math.random() * 11) + Math.floor(Math.random() * 11);
-        console.log("alt1 equalled answer so changing")
-    }
-
-    while (alt2 == answer || alt2 == alt1) {
-        alt2 = Math.floor(Math.random() * 11) + Math.floor(Math.random() * 11);
-        console.log("alt2 equalled answer so changing")
-    }
-
-    // form array for answer and alternate options
-    let options = [answer, alt1, alt2];
+    import { onMount } from 'svelte';
+    import GridBox from './GridBox.svelte';
 
     // function to shuffle an array
     // https://javascript.info/task/shuffle
@@ -59,11 +53,6 @@
         }
     }
 
-    // shuffle the options array
-    shuffle(options);
-
-    console.log(`${a} + ${b} = ${answer}`);
-
     // validate answer function
     function validateAnswer(e) {
         if (e.target.value == answer) {
@@ -72,5 +61,46 @@
             alert("INCORRECT");
         }
     }
+
+    // initialize a and b as random ints between 1 and 10
+    let a = Math.floor(Math.random() * 11);
+    let b = Math.floor(Math.random() * 11);
+
+    // calculate alternate options in same manner as a and b
+    let alt1 = Math.floor(Math.random() * 11) + Math.floor(Math.random() * 11);
+    let alt2 = Math.floor(Math.random() * 11) + Math.floor(Math.random() * 11);
+
+    // calculate answer
+    $: answer = a + b;
+
+    // form array for answer and alternate options
+    $: options = [answer, alt1, alt2];
+
+    $: console.log(`${a} + ${b} = ${answer}`);
+
+    onMount(() => {
+        // set a and b to 1 if they are 0
+        if (a == 0) a = 1;
+        if (b == 0) b = 1;
+
+        // check they are not 0
+        if (alt1 == 0) alt1 = 1;
+        if (alt2 == 0) alt2 = 1;
+
+        // check that alternate options do not equal answer
+        while (alt1 == answer || alt1 == alt2) {
+            alt1 = Math.floor(Math.random() * 11) + Math.floor(Math.random() * 11);
+            console.log("alt1 equalled answer so changing")
+        }
+
+        while (alt2 == answer || alt2 == alt1) {
+            alt2 = Math.floor(Math.random() * 11) + Math.floor(Math.random() * 11);
+            console.log("alt2 equalled answer so changing")
+        }
+
+        // shuffle the options array
+        shuffle(options);
+
+    })
 
 </script>
---
+++ src/GridBox.svelte
@@ -0,0 +1,39 @@
+<script>
+    // number of cards to draw in the grid
+    export let cards;
+</script>
+
+<section>
+    <!-- https://github.com/sveltejs/svelte/issues/2968 -->
+    {#each {length: cards} as card}
+        <div>placeholder</div>
+    {/each}
+</section>
+
+<style>
+    section {
+        display: grid;
+        gap: 1rem;
+        grid-template-columns: repeat(auto-fill, minmax(240px, 1fr));
+    }
+
+    div {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        background: #353535;
+        font-size: 2rem;
+        color: #fff;
+        box-shadow: rgba(3, 8, 20, 0.1) 0px 0.15rem 0.5rem, rgba(2, 8, 20, 0.1) 0px 0.075rem 0.175rem;
+        height: 100%;
+        width: 100%;
+        border-radius: 4px;
+        transition: all 500ms;
+        overflow: hidden;
+
+        background-size: cover;
+        background-position: center;
+        background-repeat: no-repeat;
+    }
+</style>
